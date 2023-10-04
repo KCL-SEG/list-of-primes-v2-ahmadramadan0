3f83cfc4dfeef94b4426c34b16e2beba116df603
Ahmad Ramadan <ahmdramad03@gmail.com>
2023-10-04 18:17:05 +0100

    Complete exercise

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError
+
     list = []
+    i = 2
+    while len(list) < number_of_primes:
+        for n in range(2, i):
+            if i % n == 0:
+                break
+        else:
+            list.append(i)
+        i += 1
     return list
